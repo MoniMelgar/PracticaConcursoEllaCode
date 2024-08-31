d40e55586f7b7de6ca8bbf5ad311b733e5855ed2
MMMelgar <127633583+MMMelgar@users.noreply.github.com>
2024-08-31 00:09:50 -0600

    evaluacion y def

--- Niveles.py
+++ Niveles.py
@@ -1,9 +1,38 @@
 # Estrategia 1: Se multiplican los dígitos de la puntuación
 # Estrategia 2 Se suman los cuadrados de los dígitos de la puntuación
 
+def multiplicacion(lista):
+    lista=[int(x) for x in lista]
+    result=lista[0]
+    for i in range(1, len(lista)):
+        result=list[i]*result
+    return [result]
 
+def exponencial(lista):
+    lista=[int(x) for x in lista]
+    result=lista[0]**2
+    for i in range(1, len(lista)):
+        result=(list[i]**2)+result
+    return [result]
 
-n=int(input("Ingrese el número de niveles por favor"))
+def evaluación(nivel):
+    A=0
+    B=0
+    print("nivel n°", nivel+1)
+    a,b=[int(x) for x in input("Por favor ingrese el intervalo de posibles puntajes para el nivel\n").split()]
+    if a==b:
+        if (multiplicacion(str(i).split()))>(exponencial(str(i).split())):
+            A+=1
+        else:
+            B+=1
+    else:
+        for i in range(a,b+1):
+            if (multiplicacion(str(i).split()))>(exponencial(str(i).split())):
+                A+=1
+            else:
+                B+=1
+    print(A,B)
+
+n=int(input("Ingrese el número de niveles por favor\n"))
 for i in range(n):
-    a,b=[int(x) for x in input("Por favor ingrese el intervalo de posibles puntajes para el nivel ").split()]
-    print (a,b)
+    evaluación(i)
